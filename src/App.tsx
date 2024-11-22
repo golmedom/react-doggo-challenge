8f98e2d7a41b70f172c905d3739885a6012026c7
Gustavo Adolfo Olmedo <gustavo.olmedo@mindera.com>
2024-11-22 15:35:20 +0000

    initial commit & add App styles

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <p>
+          <span role="img" aria-label="heart"></span>
+          <span role="img" aria-label="dog"></span>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
